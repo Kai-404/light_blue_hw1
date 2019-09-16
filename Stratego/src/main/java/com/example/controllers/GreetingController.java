7670dbda352a2fbd735bfc7094b3e1746604ae36
Yun Seob Lee <fhzotxldj@gmail.com>
2019-09-16 16:45:03 -0400

    add index page

---
+++ Stratego/src/main/java/com/example/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class GreetingController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
