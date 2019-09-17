8dccba668f8a67776e44317d8c5ce1356bbc295d
Yun Seob Lee <fhzotxldj@gmail.com>
2019-09-16 20:39:48 -0400

    add database for user table

--- Stratego/src/main/java/com/example/demo/DemoApplication.java
+++ Stratego/src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.example.controllers"})
+@EnableMongoRepositories(basePackages = "com.example.model")
+@SpringBootApplication(scanBasePackages = {"com.example.controllers", "com.example.model"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- Stratego/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Stratego/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,33 @@
 package com.example.demo;
 
+import com.example.model.User;
+import com.example.model.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration(classes = {DemoApplication.class})
+@DataMongoTest
+
 public class DemoApplicationTests {
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void dbTest() {
+		userRepository.deleteAll();
+		userRepository.save(new User("pikapika", "abs"));
+		assert userRepository.findByUserId("pikapika") != null;
+	}
 }
