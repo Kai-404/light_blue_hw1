d1a62f0c59abca83561ee48de910d9e279919e51
Yun Seob Lee <fhzotxldj@gmail.com>
2019-09-18 01:56:18 -0400

    integrate react with spring boot

---
+++ Stratego/frontend/src/pages/register.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
+import "../App.css";
+
+class Register extends Component {
+  state = {
+    username: "",
+    email: "@stonybrook.edu",
+    password: "",
+    password2: "",
+    phoneNumber: ""
+  };
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+  /** 
+  onSubmit = e => {
+    e.preventDefault();
+    const { name, email, password, password2, phoneNumber } = this.state;
+    if (password !== password2) {
+      this.props.createAccount(name, email, "", phoneNumber);
+    } else this.props.createAccount(name, email, password, phoneNumber);
+    this.setState({
+      name: "",
+      email: "@stonybrook.edu",
+      password: "",
+      password2: "",
+      phoneNumber: ""
+    });
+    this.props.history.push("/");
+  };*/
+  render() {
+    return (
+      <form className="form" onSubmit={this.onSubmit}>
+        User Name:
+        <input
+          className="input"
+          placeholder="UserName"
+          value={this.state.name}
+          type="text"
+          name="name"
+          onChange={this.onChange}
+        />
+        <br />
+        <br />
+        Email:
+        <input
+          className="input"
+          placeholder="first.last@stonybrook.edu"
+          value={this.state.email}
+          type="text"
+          name="email"
+          onChange={this.onChange}
+        />
+        <br />
+        <br />
+        Password:
+        <input
+          className="input"
+          value={this.state.password}
+          type="password"
+          name="password"
+          onChange={this.onChange}
+        />
+        <br />
+        <br />
+        Reenter Password:
+        <input
+          className="input"
+          value={this.state.password2}
+          type="password"
+          name="password2"
+          onChange={this.onChange}
+        />
+        <br />
+        <br />
+        Phone Number:
+        <input
+          className="input"
+          placeholder={"1234567890"}
+          value={this.state.phoneNumber}
+          type="text"
+          name="phoneNumber"
+          onChange={this.onChange}
+        />
+        <br />
+        <br />
+        <button type="submit" className="submitButton">
+          Submit
+        </button>
+        {"  "}
+        <button type="button" className="submitButton">
+          <Link className="linkStyle" to="/">
+            Cancle
+          </Link>
+        </button>
+      </form>
+    );
+  }
+}
+
+//propTypes
+Register.propTypes = {
+  Register: PropTypes.func.isRequired
+};
+
+export default withRouter(Register);
