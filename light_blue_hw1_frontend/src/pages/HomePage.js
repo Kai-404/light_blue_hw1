89232a7e87dadf0f2a2d5f2684d10041e18bb8e5
Jiayi <Jiayi.Gao@stonybrook.ed>
2019-09-17 22:17:35 -0400

    changed homepage

--- light_blue_hw1_frontend/src/pages/HomePage.js
+++ light_blue_hw1_frontend/src/pages/HomePage.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
+import "../App.css";
 
 class HomePage extends Component {
   render() {
     return (
       <React.Fragment>
-        <p>Welcome to the light blue online Stratego</p>
-        <br />
-        <button>play</button>
-        <p> Game holder</p>
+        <p className="notes">
+          Welcome to the light blue online{" "}
+          <a href="https://www.wikihow.com/Play-Stratego">Stratego</a>
+          . You can click on the play button to play the game online <br />
+          Note: if you want to record all your game results please have an
+          account and logged in
+        </p>
+
+        <button type="button" className="playBotton">
+          Game holder play
+        </button>
+        <br></br>
       </React.Fragment>
     );
   }
