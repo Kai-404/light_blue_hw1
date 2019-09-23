8ba66662a53d10d8033029a86552ac35e242eb36
Jiayi <Jiayi.Gao@stonybrook.ed>
2019-09-23 11:41:49 -0400

    integrated with react axios

--- Stratego/frontend/src/game/Game.js
+++ Stratego/frontend/src/game/Game.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import axios from "axios";
+
 import "../App.css";
 import "./Game.css" ;
 
@@ -14,6 +16,7 @@ class Game extends React.Component {
   if a move made, return state
   set state of board
   set state of turn
+  ifValidMove()
   determine winingState/winner
   */
   constructor(props) {
@@ -22,30 +25,31 @@ class Game extends React.Component {
       PLAYER_ONE: "R",
       PLAYER_TWO: "B",
       currentTurn: "R",
+      FIRST_SELECT: null,
+      SECOND_SELECT: null,
       //board will be load from the back end as a list/array
       board: Array(100).fill(";)"),
       winner: null
     };
   }
-  /**
+
   componentDidMount() {
     //get init game data from spring boot
-    axios.get("/boardState").then(res => this.setState({ users: res.data }));
+    axios.get("/game/init").then(res => this.setState({ board: res.data }));
   }
-  */
+
   playGame = () => {};
   setupGame = () => {};
   surrender = () => {};
 
+  //at most 2 buttons can be selected, and check if second is a valid move
+  //if not, unclick 2nd
+  //if valid, swap value according to index.
+
   handleClick(index) {
-    this.state.board[index] = "O";
-    this.setState({
-      board: this.state.board
-    });
   }
 
   render() {
-    //css doesn't work!!!!!!!!!!why
     let board = this.state.board.map((cell, index) => {
       return (
         <button onClick={() => this.handleClick(index)} className="square">
